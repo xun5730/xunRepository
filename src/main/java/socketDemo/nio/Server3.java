b05ed51e81a66bda050be4207964fd06afc4fce6
xun <xun@windows10.microdone.cn>
2018-09-27 22:37:13 +0800

    提交NIO例子

--- src/main/java/socketDemo/nio/Server3.java
+++ src/main/java/socketDemo/nio/Server3.java
@@ -7,6 +7,8 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+
 
 public class Server3 implements Runnable {
 
@@ -14,29 +16,28 @@ public class Server3 implements Runnable {
 	private Selector selector;
 	//2 建立缓冲区
 	private ByteBuffer readBuf=ByteBuffer.allocate(1024);
-	//3
-	private ByteBuffer writeBuf=ByteBuffer.allocate(1024);
+	//3 返回客户端多的 buff  这里没有实现
+//	private ByteBuffer writeBuf=ByteBuffer.allocate(1024);
 	
 	public Server3(int port){
 		
 		try {
-			//1打开路服用器
-			this.selector=Selector.open();
-			//2打开 服务器通道
-			ServerSocketChannel ssc=ServerSocketChannel.open();
-			//3设置服务器通道为非阻塞模式
+			//1 打开路复用器
+			this.selector = Selector.open();
+			//2 打开服务器通道
+			ServerSocketChannel ssc = ServerSocketChannel.open();
+			//3 设置服务器通道为非阻塞模式
 			ssc.configureBlocking(false);
-			//4绑定地址
+			//4 绑定地址
 			ssc.bind(new InetSocketAddress(port));
-			//5把服务器通过注册到多路复用器上，并且监听阻塞事件
+			//5 把服务器通道注册到多路复用器上，并且监听阻塞事件
 			ssc.register(this.selector, SelectionKey.OP_ACCEPT);
-			System.out.println("server start ,port:"+port);
+			
+			System.out.println("Server start, port :" + port);
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 	}
 	
 	
@@ -44,26 +45,96 @@ public class Server3 implements Runnable {
 	
 	@Override
 	public void run() {
+		while(true){
+			
 		
-	}
-	
-	private void accept(SelectionKey key){
-		
-		     try {
-		    	//1获取服务通道
-		 		ServerSocketChannel ssc=(ServerSocketChannel) key.channel();
-		 		//2执行阻塞方法
-				SocketChannel sc=  ssc.accept();
-				//3设置阻塞的模式
-				sc.configureBlocking(false);
-				//4注册到多路复用器上，并设置读取标志
-				sc.register(this.selector, SelectionKey.OP_READ);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		try {
+			//1 必须要让多路复用器开始监听
+			this.selector.select();
+			//2 返回多路复用器已经选择的结果集
+			Iterator<SelectionKey> keys=   this.selector.selectedKeys().iterator();
+			//3 进行遍历
+			while(keys.hasNext()){
+				//4 获取一个选择的元素
+				SelectionKey key=  keys.next();
+				//5 直接从容器中移除就可以了
+				keys.remove();
+				//6 如果是有效的
+				if(key.isValid()){
+					//7 如果为阻塞状态
+					if(key.isAcceptable()){
+						this.accept(key);
+					}
+					//如果为可读状态
+					if(key.isReadable()){
+						this.read(key);
+					}
+					//9 写数据
+					if(key.isWritable()){
+						//this.write(key); //ssc
+					}
+				}
 			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		}
 	}
 	
+	private void read(SelectionKey key) {
+		try{
+			//1 清空缓冲区旧的数据
+			this.readBuf.clear();
+			//2 获取之前注册的socket通道对象
+			SocketChannel sc=  (SocketChannel) key.channel();
+			//3 读取数据
+			int count= sc.read(this.readBuf);
+			//4 如果没有数据
+			if(count==-1){
+				key.channel().close();
+				key.cancel();
+				return ;
+			}
+			
+			//5 有数据则进行读取 读取之前需要进行复位方法(把position 和limit进行复位)
+			this.readBuf.flip();
+			//6 根据缓冲区的数据长度创建相应大小的byte数组，接收缓冲区的数据
+			byte[] bytes=new byte[this.readBuf.remaining()];
+			//7 接收缓冲区数据
+			this.readBuf.get(bytes);
+			//8 打印结果
+			String body=new String(bytes).trim();
+			System.out.println("server"+body);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+
+
+
+	private void accept(SelectionKey key){
+		try{
+		//1 获取服务通道
+		ServerSocketChannel ssc=	(ServerSocketChannel) key.channel();
+		//2 执行阻塞方法
+		SocketChannel sc=  ssc.accept();
+		//3 设置阻塞模式
+		sc.configureBlocking(false);
+		//4 注册到多路复用器上，并设置读取标识
+		sc.register(this.selector, SelectionKey.OP_READ);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		
+		new Thread(new Server3(8765)).start();;
+		
+	}
 	
 	
 
